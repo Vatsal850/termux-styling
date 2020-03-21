17b6aafc0aca6d6b05579d22f145c4843bc554f0
Vatsal850 <53715295+Vatsal850@users.noreply.github.com>
2020-03-21 12:25:42 +0530

    Delete Style.sh

--- Style.sh
+++
@@ -1,8 +0,0 @@
-apt-get update -y
-apt-get upgrade -y
-clear && echo """ 
-         |"""""" ----- \\.  // ||        EEEEEEE
-         |_____.   |    \\.//  ||        EE    
-               |   |      //   ||        EEEEEEE
-         """""""   |     //    |||||||   EE
-                                         EEEEEEE """
